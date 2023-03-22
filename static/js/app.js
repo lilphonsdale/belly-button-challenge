9ce01f80d07edee6e21306588bffcf7575c09be4
lilphonsdale <118135886+lilphonsdale@users.noreply.github.com>
2023-03-21 18:29:39 -0700

    little more progress
    - dropdown menu and graph

--- static/js/app.js
+++ static/js/app.js
@@ -6,17 +6,35 @@ const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 
 function init(){
 
-// Fetch the JSON data and console log it
+// Fetch the JSON data 
 
 d3.json(url).then(function (data) {
   // let metadata = data.metadata
   let names = data.names
   let samples = data.samples
+  // console.log(names);
+  let sample_values = samples.map(function (x) {
+    return x.sample_values;
+  });
+  console.log(sample_values)
+
+  // // Use otu_ids as the labels for the bar chart.
+
+  let otu_ids = samples.map(function (x) {
+  return x.otu_ids;
+  });
+
+  // // Use otu_labels as the hovertext for the chart.
+
+  let otu_labels = samples.map(function (x) {
+  
+    return x.otu_labels;
+  });
 
 // Use the names to populate the dropdown menu
 
   var choices = d3.select("#selDataset");
-  Object.entries(names).forEach(([k, v]) => {
+  Object.entries(names).forEach(([k,v]) => {
     choices.append("option").attr("value", v).text(v)});
 
   choices.onchange = function () {
@@ -27,54 +45,76 @@ d3.json(url).then(function (data) {
   };
 
 
+// make a first plot to greet us on the page
+
+function create_plot(d){
+  plot_data = [
+      {
+          x: sample_values[d],
+          y: otu_ids[d],
+          labels: otu_labels[d],
+          type: "bar"
+      }]
+  layout = {
+        title: "Graph"
+  }
+  Plotly.newPlot("bar", plot_data, layout);
+}
+// function update_plot(d){
+//   let values = Object.values(d);
+//   let labels =  Object.keys(d);
+//   Plotly.restyle("pie", "values", [values]);
+//   Plotly.restyle("pie", "labels", [labels]);
+// }
+
+create_plot(0);
 });
 }
 
+init()
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildMetadata(newSample);
-  buildCharts(newSample); 
-}
+// function optionChanged(newSample) {
+//   // Fetch new data each time a new sample is selected
+//   buildMetadata(newSample);
+//   buildCharts(newSample); 
+// }
 
-// // Display the default plot
-//   // function init() {
-//   let d = [{
-//     values: sample_values[0],
-//     labels: otu_ids[0],
-//     type: "bar"
-//   }];
+// Display the default plot
+  // function init() {
+  // let d = [{
+  //   values: sample_values[0],
+  //   labels: otu_ids[0],
+  //   type: "bar"
+  // }];
 
-//   let layout = {
-//     height: 600,
-//     width: 800
-//   };
+  
+  // };
 
-//   Plotly.newPlot("bar", d, layout);
-//   // };
+  // Plotly.newPlot("bar", d, layout);
+  // // };
 
 // Demographics Panel 
-function buildMetadata(sample) {
-  d3.json(url).then((data) => {
-    var metadata = data.metadata;
-    // Filter the data for the object with the desired sample number
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    // Use d3 to select the panel with id of `#sample-metadata`
-    var PANEL = d3.select("#sample-metadata");
+// function buildMetadata(sample) {
+//   d3.json(url).then((data) => {
+//     var metadata = data.metadata;
+//     // Filter the data for the object with the desired sample number
+//     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+//     var result = resultArray[0];
+//     // Use d3 to select the panel with id of `#sample-metadata`
+//     var PANEL = d3.select("#sample-metadata");
 
-    // Use `.html("") to clear any existing metadata
-    PANEL.html("");
+//     // Use `.html("") to clear any existing metadata
+//     PANEL.html("");
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-    Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    });
+//     // Use `Object.entries` to add each key and value pair to the panel
+//     // Hint: Inside the loop, you will need to use d3 to append new
+//     // tags for each key-value in the metadata.
+//     Object.entries(result).forEach(([key, value]) => {
+//       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+//     });
 
-  });
-}
+//   });
+// }
 
 
 // var choices = d3.select("#selDataset");
